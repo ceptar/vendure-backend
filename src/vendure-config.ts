3eaf8aac44ee91c2bbc0f3275a741c5ab9c0f14b
ceptar <christoph.cerjan@gmail.com>
2026-01-24 14:55:27 +0100

    x

--- src/vendure-config.ts
+++ src/vendure-config.ts
@@ -78,6 +78,8 @@ export const config: VendureConfig = {
         connection: {
           host: process.env.REDIS_HOST || "localhost",
           port: +(process.env.REDIS_PORT || 6379),
+          username: process.env.REDIS_USERNAME || undefined,
+          password: process.env.PASSWORD || undefined,
           maxRetriesPerRequest: null,
         },
       }),
