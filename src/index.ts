977afe1777120553e264d1f0f679f4075b5127aa
ceptar <christoph.cerjan@gmail.com>
2026-01-26 18:34:20 +0100

    x

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import {bootstrap,
     // JobQueueService, 
     runMigrations} from '@vendure/core';
 import { config } from './vendure-config';
-import { populateOnFirstRun } from './populate';
+// import { populateOnFirstRun } from './populate';
 
-populateOnFirstRun(config)
-     .then(() => runMigrations(config))
-// runMigrations(config)
+// populateOnFirstRun(config)
+//     .then(() => runMigrations(config))
+ runMigrations(config)
     .then(() => bootstrap(config))
     // .then(app => {
     //     // For "lite" deployments with limited resources, we can run the job queue
